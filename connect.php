a2fa84916e244df301d8865725149c8306078963
pipe <6322771633@g.siit.tu.ac.th>
2022-10-28 00:23:58 +0700

    Update connect.php
    @Isariyafxk

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost','root','','staff');
+$mysqli = new mysqli('localhost','root','','pet_hospital');
    if($mysqli->connect_errno){
       echo $mysqli->connect_errno.": ".$mysqli->connect_error;
    }
