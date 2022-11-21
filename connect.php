0e0ce042f5d05f7144aec277d4812e2c5c99f028
pipe <6322771633@g.siit.tu.ac.th>
2022-11-21 08:32:35 +0700

    update web and css

--- connect.php
+++ connect.php
@@ -2,5 +2,7 @@
 $mysqli = new mysqli('localhost','root','','pet_hospital');
    if($mysqli->connect_errno){
       echo $mysqli->connect_errno.": ".$mysqli->connect_error;
-   }
+   } #test
  ?>
+
+
